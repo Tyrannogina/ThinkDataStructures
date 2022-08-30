edb8319841d272904e2cafdf33374242ebad6e35
tyrannogina <tyrannogina@gmail.com>
2022-08-30 11:22:51 +0100

    Fix error in original code

--- code/src/com/allendowney/thinkdast/WikiNodeExample.java
+++ code/src/com/allendowney/thinkdast/WikiNodeExample.java
@@ -16,21 +16,20 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiNodeExample {
-	
+
 	public static void main(String[] args) throws IOException {
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		
+
 		// download and parse the document
 		Connection conn = Jsoup.connect(url);
 		Document doc = conn.get();
-		
+
 		// select the content text and pull out the paragraphs.
 		Element content = doc.getElementById("mw-content-text");
-				
 		// TODO: avoid selecting paragraphs from sidebars and boxouts
 		Elements paras = content.select("p");
-		Element firstPara = paras.get(0);
-		
+		Element firstPara = paras.get(1);
+
 		recursiveDFS(firstPara);
 		System.out.println();
 
